bb34d7c936c9cbcaa17626bc2ef51ad5f59cea35
braunAvi <avibraun@zoho.com>
2017-05-16 22:09:28 +0100

    adding review edit page

--- readings/views.py
+++ readings/views.py
@@ -11,7 +11,6 @@ from django.core.urlresolvers import reverse
 def review_list(request):
     reviews = Review.objects.all()
     reviews = Review.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-
     return render(request,
                   'readings/review_list.html',
                   {'reviews': reviews})
@@ -49,3 +48,22 @@ def book_new(request):
     else:
         form = BookForm()
     return render(request, 'readings/book_edit.html', {'form': form})
+
+def review_edit(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.published_date = timezone.now()
+            review.save()
+            return redirect('readings:review_detail', review_id=review.pk)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'readings/review_edit.html', {'form': form})
+
+
+
+# def review_edit(request, pk):
+#     i=2
